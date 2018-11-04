b61dc0c180a1b59f7fe910d635205cc39d994c9d
Justin Kuepper <justin.kuepper@gmail.com>
2018-11-04 15:28:17 -0700

    added rating blueprint

--- reciperoulette/__init__.py
+++ reciperoulette/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'recipe-roulette.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'reciperoulette.sqlite'),
     )
 
     if test_config is None:
@@ -34,5 +34,9 @@ def create_app(test_config=None):
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import rating
+    app.register_blueprint(rating.bp)
+    app.add_url_rule('/create', endpoint='index') # This needs to be updated when index is ready...
 
     return app
---
+++ reciperoulette/rating.py
@@ -0,0 +1,37 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from reciperoulette.auth import login_required
+from reciperoulette.db import get_db
+
+bp = Blueprint('rating', __name__)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        rating = request.form['rating']
+        recipe_id = request.form['recipe']
+        error = None
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO rating (recipe_id, user_id, rating)'
+                ' VALUES (?, ?, ?)',
+                (recipe_id, g.user['id'], rating)
+            )
+            db.commit()
+            return redirect('/create')
+    else:
+        db = get_db()
+        recipe = db.execute(
+            'SELECT * FROM recipe r WHERE r.id'
+            ' NOT IN (SELECT recipe_id FROM rating WHERE user_id = ?)'
+            ' ORDER BY RANDOM() LIMIT 1',
+            (g.user['id'],)
+        ).fetchone()
+        return render_template('rating/index.html', recipe=recipe)
